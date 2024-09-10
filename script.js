4b8a1cd118b6c570f692d2cde5056f36602ab09c
stellasuh <sohazainalabiden@gmail.com>
2024-09-10 20:42:49 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+window.onload = function() {
+    const quotes = [
+        {
+            text: "“Many people are afraid to take challenge because they don’t know what they can do but i want to tell them that a new door can be open if they want to to try it. \nI’m always cheering for you” ",
+            author: "Wonwoo",
+            image: "https://i.pinimg.com/564x/66/e1/dc/66e1dcec354631ed6caff4df655f52cd.jpg"
+        },
+        {
+            text: "“Life will always be filled with stressful moments, but you can always choose to see something positive through it all. \nIt’ll be okay. You’ll be okay.”",
+            author: "Joshua",
+            image: "https://i.pinimg.com/564x/a9/46/52/a946522413b9c17490ee686a6c4034dc.jpg"
+        },
+        {
+            text: "“Reality is more cruel than our imagination. But we are stronger than the person we imagine ourselves to be.”",
+            author: "The8",
+            image: "https://i.pinimg.com/564x/f5/fc/77/f5fc773f5a6ffc65f48e19ef8e6b52a8.jpg"
+        }
+    ];
+
+    const colors = ["#e66a8e", "#ecadc8", "#e98dad", "#ffbdd0"];
+    let lastIndex = -1;  // To track the last shown quote
+
+    // Function to get a random index, making sure it's not the same as the last index
+    function getRandomIndex(arrLength, currentIndex) {
+        let randomIndex;
+        do {
+            randomIndex = Math.floor(Math.random() * arrLength);
+        } while (randomIndex === currentIndex); // Avoid repeating the same quote
+        return randomIndex;
+    }
+
+    // Function to update the quote, author, image, and background color
+    function updateQuote() {
+        const randomIndex = getRandomIndex(quotes.length, lastIndex);
+        lastIndex = randomIndex;
+
+        const randomQuote = quotes[randomIndex];
+        const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+        // Update quote, author, image, and background color
+        document.getElementById('quoteDisplay').innerText = randomQuote.text;
+        document.getElementById('authorDisplay').innerText = "- " + randomQuote.author;
+        document.getElementById('quoteImage').src = randomQuote.image;
+        document.getElementById('quoteContainer').style.backgroundColor = randomColor;
+    }
+
+    // Change quote every 5 seconds (5000 ms)
+    setInterval(updateQuote, 5000);
+
+    // Initial quote display
+    updateQuote();
+};
