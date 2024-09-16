21275e2a9b769290cd1a352e01467a34a36b9388
stellasuh <sohazainalabiden@gmail.com>
2024-09-16 22:42:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ window.onload = function() {
             text: "“Life will always be filled with stressful moments, but you can always choose to see something positive through it all. \nIt’ll be okay. You’ll be okay.”",
             author: "Joshua",
             image: "https://i.pinimg.com/564x/a9/46/52/a946522413b9c17490ee686a6c4034dc.jpg"
+            audio: "You'll be okay.mp3"
         },
         {
             text: "“Reality is more cruel than our imagination. But we are stronger than the person we imagine ourselves to be.”",
@@ -29,6 +30,13 @@ window.onload = function() {
             text: "“You’ve probably heard of this... -There is no flower that blooms without being shaken-. So i hope that all of you will endure hardship and overcome difficulties to bear fruits. I don’t want you to try to live up to other people’s expectations. I want you to live by your own values and take your own small steps forward, one at a time.”",
             author: "Hoshi",
             image: "https://i.pinimg.com/564x/73/2e/53/732e5331c77b15470707f94967deb3d5.jpg"
+            audio: "Hoshimotivation.mp3"
+        },
+        {
+            text: "“Don’t worry too much! Well you can worry a little, but it’ll be okay! Let’s take on challenges and grow together. So everyone, even when there’s hardships let’s try to think of something good. Not like it's just going to be better, but within this reality and situation. Okay? /nSo everyone, let’s have another nice day tomorrow. /nFighting! Don’t skip your meals.”",
+            author: "Dino",
+            image: "https://i.pinimg.com/564x/f5/20/98/f52098007775e09fd5376333303f929f.jpg"
+            audio: "Dinooo.mp3"
         },
     ];
 
